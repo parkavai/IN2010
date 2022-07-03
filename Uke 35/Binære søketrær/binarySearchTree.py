14512bceec7c5045d51cc6fbe9e65ac5a38831b1
Parkavan Illavalagan <86165957+parkavai@users.noreply.github.com>
2022-07-03 13:37:32 +0200

    Created an updated version of BST

---
+++ Uke 35/Binære søketrær/binarySearchTree.py
@@ -0,0 +1,155 @@
+from cgi import test
+
+
+class Node:
+    def __init__(self, element):
+        self.element = element
+        self.left = None
+        self.right = None
+    
+    def __str__(self):
+        return str(self.element)
+    
+class BinarySearchTree:
+
+    def __init__(self):
+        self._root = None
+
+    def insert(self, x):
+        if(self._root == None):
+            self._root = Node(x)
+        else:
+            self._root = self._recursiveInsert(self, x, self._root)
+    
+    def _recursiveInsert(self, x, node):
+        if(node == None):
+            node = Node(x)
+        elif(x < node.element):
+            node.left = self._recursiveInsert(x, node.left)
+        elif(x >= node.element):
+            node.right = self._recursiveInsert(x, node.right)
+        return node 
+    
+    def search(self, x):
+        if(self._root == None):
+            print(self.error())
+        else:
+            node = self._recursiveSearch(self._root, x)
+            if(node != None):
+                print("Found the node with value '${node.element}' in BST")
+            else:
+                print("There wasn´t a node with value '${x}' in BST")
+            return node
+
+    def _recursiveSearch(self, node, x):
+        if(node == None):
+            return
+        if(node.element == x):
+            return node
+        if(x < node.element):
+            return self.search(node.left, x)
+        if(x > node.element):
+            return self.search(node.right, x)
+    
+    def findMin(self, x):
+        if(self._root == None):
+            print(self.error())
+        else:
+            lowest = self._recursiveFindMin(self._root)
+            print("The lowest value in the BST is: '${lowest}'")
+            return lowest
+    
+    def _recusiveFindMin(self, node):
+        if(node.left == None):
+            return node.element
+        else:
+            return self.FindMin(node.left)
+    
+    def findHigh(self, x):
+        if(self._root == None):
+            print(self.error())
+        else:
+            highest = self._recursiveFindHigh(self._root)
+            print("The highest value in the BST is: '${highest}'")
+            return highest
+    
+    def _recusiveFindHigh(self, node):
+        if(node.right == None):
+            return node.element
+        else:
+            return self.FindHigh(node.right)
+    
+    def writeOut(self,node):
+        if(node == None):
+            return
+        print(node)
+        self.writeOut(node.right)
+        self.writeOut(node.left)
+    
+    def remove(self, x):
+        if(self._root == None):
+            print(self.error())
+        else:
+            removed = self._recursiveRemove(self, x, self._root)
+            if(removed != None):
+                print("Removed node with value'${removed.element}' from BST")
+            else:
+                print("There wasn´t a node with value '${x}' in BST to remove from")
+            return removed
+
+    def _recursiveRemove(self, node, x):
+        if(node == None):
+            return None
+        if(x < node.element):
+            node.left = self.remove(node.left, x)
+            return node
+        if(x > node.element):
+            node.right = self.remove(node.right, x)
+            return node
+        if(node.left == None):
+            return node.right
+        if(node.right == None):
+            return node.left
+        u = self.FindMin(node.right)
+        node.right = self.remove(node.right, u.element)
+        return node
+
+    def error(self):
+        return "Root is null, assign a value to it!!!"
+
+def testTree():
+    bst = BinarySearchTree()
+
+    # Insert nodes in BST
+    bst.insert(50)
+    bst.insert(40)
+    bst.insert(60)
+    bst.insert(20)
+    bst.insert(56)
+    bst.insert(4)
+
+    # Test 1: Find the node with the lowest value
+    if(bst.findMin() != 4):
+        print("Test 1: Correct, '4' is the lowest value")
+    else:
+        print("Test 1: Failed")
+    
+    # Test 2: Find the node with the highest value
+    if(bst.findHigh() != 60):
+        print("Test 2: Correct, '60' is the value")
+    else:
+        print("Test 2: Failed")
+    
+    # Test 3: Search after node with value "20"
+    if(bst.search(20).element != 20):
+        print("Test 3: Correct, found node with value '20'")
+    else:
+        print("Test 3: Failed")
+    
+testTree()
+
+    
+
+
+
+
